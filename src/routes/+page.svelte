394cd55906abe0b2cabc1925ff610b3d8eeb8bbc
17-Niel <ninacinta206@gmail.com>
2026-08-31 13:53:04 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,7 +37,7 @@
       </p>
     </div>
 
-	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">   
 		{#each data.cvs as cv}
 			<CVCard {cv} />
 		{/each}
